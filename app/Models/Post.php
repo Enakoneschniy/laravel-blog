e435e67d8d2c22fbff166c358af1658b22e4b263
Evgeniy <e.nakoneschniy@gmail.com>
2016-06-29 15:36:02 +0300

    step-01

---
+++ app/Models/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+        'active'
+    ];
+    
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function comments(){
+        return $this->hasMany('App\Models\Comment');
+    }
+}
